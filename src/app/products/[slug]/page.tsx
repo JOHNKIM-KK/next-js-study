4a0e00115e2cc82ecac6a90f64684bc2095834dd
김명준 <kmj@itsdcode.com>
2023-08-01 17:48:35 +0900

    (feat): 정적 라우팅 , 동적라우팅, not-found 페이지

---
+++ src/app/products/[slug]/page.tsx
@@ -0,0 +1,22 @@
+import { notFound } from "next/navigation";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function PantsPage({ params }: Props) {
+  if (params.slug === "noting") {
+    notFound();
+  }
+
+  return <h1>{params.slug} 페이지</h1>;
+}
+
+export function generateStaticParams() {
+  const products = ["pants", "skirt"];
+  return products.map((product) => ({
+    slug: product,
+  }));
+}

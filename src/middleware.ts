c880efd47e5568e07a1cbeb2760941ba764e5049
김명준 <kmj@itsdcode.com>
2023-08-02 20:41:45 +0900

    (feat): 미들웨어 생성 및 적용

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  console.log("mj : ㅁㅣ들웨어 실행중");
+  if (request.nextUrl.pathname.startsWith("/products/1004")) {
+    console.log("mj : ㅁㅣ들웨어 경로 리다이렉트");
+
+    return NextResponse.redirect(new URL("/products", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/products/:path*"],
+};

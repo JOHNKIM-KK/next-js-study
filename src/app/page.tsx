b40931e710739da89e1e645e90ba981c945c73c4
김명준 <acspp2017@gmail.com>
2024-10-15 21:00:34 +0900

    feat: 버전3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Counter from "@/components/Counter";
 export default function Home() {
   return (
     <div>
-      <h1>홈페이지 버전 2</h1>
+      <h1>홈페이지 버전 3</h1>
       <Counter />
     </div>
   );
